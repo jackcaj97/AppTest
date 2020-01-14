4f81e60bd061b76e478af7e088ab9ca43ccc09e1
zioposty <l.postiglione4@studenti.unisa.it>
2020-01-14 11:12:29 +0100

    app name

--- AppTest/AppTest.Android/MainActivity.cs
+++ AppTest/AppTest.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Xamarin.Auth;
 
 namespace AppTest.Droid
 {
-    [Activity(Label = "AppTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AnimalGuess", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
